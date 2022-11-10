9bcfd8c4fcae70f974cd8fc74f09913473395e77
Shawn Rizo <shawnrizo@hotmail.com>
2022-11-10 15:59:05 -0500

    (Chore) Inputted LSP Parameters

--- index.sh
+++ index.sh
@@ -6,10 +6,26 @@
 # This is a script to automate LSP deployments
 
 # INPUTS
-node=#!/usr/bin/env node
-GAS_PRICE=10
-URL=
-MNEMONIC=
-Pair_Name=
-expirationTimestamp=
-collateralPerPair=
+# Mandatory LSP Parameters:
+
+node = index.js #!/usr/bin/env node   : index.js?
+gasPrice = 10
+url = https://rpc.sx.technology/
+mnemonic = robust truck circle pause future pilot oxygen fog peace drastic knock language
+pairName = $firstname $lastname APT Pair
+expirationTimestamp = 1676332799
+collateralPerPair = 1000000000000000000000
+priceIdentifier = APT
+longSynthName = $firstname $lastname Long APT
+longSynthSymbol = $f$lLAPT
+shortSynthName = $firstname $lastname Short APT
+shortSynthSymbol = $f$lSAPT
+collateralToken = 0xd9Fd6e207a2196e1C3FEd919fCFE91482f705909
+FPL = Linear
+lowerBound = 0
+upperBound = 1000000000000000000000
+proposerReward = 20000000000000000000
+optimisticOracleProposerBond = 40000000000000000000
+
+$f = ""(Empty)       #first ch of firstname
+$l = ""(Empty)       #first ch of lastname
